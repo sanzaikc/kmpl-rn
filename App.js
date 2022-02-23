42c718e9f60b2725d60d62ed16e7e7e0e7d6fde6
Sanjay Khatri <sanzaikc@gmail.com>
2022-02-23 13:11:02 +0545

    native base inititated

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { NativeBaseProvider, Box } from "native-base";
 
 import LoginScreen from "./src/screens/LoginScreen";
 
 export default function App() {
   return (
-    <>
+    <NativeBaseProvider>
       <LoginScreen />
-    </>
+    </NativeBaseProvider>
   );
 }
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,13 +1,90 @@
 import React from "react";
 import { StyleSheet, Text } from "react-native";
-
-import Container from "../components/shared/Container";
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  Heading,
+  HStack,
+  Input,
+  Link,
+  VStack,
+} from "native-base";
 
 export default function LoginScreen() {
   return (
-    <Container>
-      <Text>LoginScreen</Text>
-    </Container>
+    <Center h="100%" w="100%">
+      <Box safeArea p="2" py="8" w="90%">
+        <Heading
+          size="lg"
+          fontWeight="600"
+          color="primary.800"
+          _dark={{
+            color: "primary.50",
+          }}
+        >
+          Welcome
+        </Heading>
+        <Heading
+          mt="1"
+          _dark={{
+            color: "primary.200",
+          }}
+          color="primary.600"
+          fontWeight="medium"
+          size="xs"
+        >
+          Sign in to continue!
+        </Heading>
+
+        <VStack space={3} mt="5">
+          <FormControl>
+            <FormControl.Label>Email ID</FormControl.Label>
+            <Input />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input type="password" />
+            <Link
+              _text={{
+                fontSize: "xs",
+                fontWeight: "500",
+                color: "indigo.500",
+              }}
+              alignSelf="flex-end"
+              mt="1"
+            >
+              Forget Password?
+            </Link>
+          </FormControl>
+          <Button mt="2" colorScheme="indigo">
+            Sign in
+          </Button>
+          <HStack mt="6" justifyContent="center">
+            <Text
+              fontSize="sm"
+              color="secondary.600"
+              _dark={{
+                color: "primary.200",
+              }}
+            >
+              I'm a new user.{" "}
+            </Text>
+            <Link
+              _text={{
+                color: "indigo.500",
+                fontWeight: "medium",
+                fontSize: "sm",
+              }}
+              href="#"
+            >
+              Sign Up
+            </Link>
+          </HStack>
+        </VStack>
+      </Box>
+    </Center>
   );
 }
 
