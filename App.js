6d3e7b406d51725b113715fe66f3f7e28e4da3f5
Sanjay Khatri <sanzaikc@gmail.com>
2022-02-24 08:28:45 +0545

    mend

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NativeBaseProvider } from "native-base";
-
 import { NavigationContainer } from "@react-navigation/native";
+
 import AppBottomNavigator from "./src/navigator/AppBottomNavigator";
 
 export default function App() {
