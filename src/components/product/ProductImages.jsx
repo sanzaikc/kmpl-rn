7466d4692c693f00cf79b035cf847eac928d02bc
Sanjay Khatri <sanzaikc@gmail.com>
2022-03-03 09:07:13 +0545

    image carousel pagination

--- src/components/product/ProductImages.jsx
+++ src/components/product/ProductImages.jsx
@@ -1,34 +1,53 @@
-import { View, FlatList, Image, useWindowDimensions } from "react-native";
-import { AspectRatio, Box } from "native-base";
+import React from "react";
+import { View, FlatList, Image, useWindowDimensions, Text } from "react-native";
+import { AspectRatio, Box, Center, HStack } from "native-base";
 
-const randomArray = Array.apply(null, { length: 3 }).map((x) => ({
+const randomArray = Array.apply(null, { length: 10 }).map((x) => ({
   id: Math.random(),
 }));
 
 export default function ProductImages() {
   const { width, height } = useWindowDimensions();
+  const [pagination, setPagination] = React.useState(1);
 
   return (
-    <FlatList
-      data={randomArray}
-      keyExtractor={({ id }) => id.toString()}
-      renderItem={(_) => (
-        <Box maxHeight={height * 0.4} width={width} overflow="hidden">
-          <AspectRatio w="100%" ratio={4 / 5}>
-            <Image
-              source={{
-                uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg",
-              }}
-              alt="image"
-              resizeMode="cover"
-            />
-          </AspectRatio>
-        </Box>
-      )}
-      horizontal
-      showsHorizontalScrollIndicator={false}
-      snapToInterval={width}
-      snapToAlignment="start"
-    />
+    <Center>
+      <FlatList
+        data={randomArray}
+        keyExtractor={({ id }) => id.toString()}
+        renderItem={(_) => (
+          <Box maxHeight={height * 0.4} width={width} overflow="hidden">
+            <AspectRatio w="100%" ratio={4 / 5}>
+              <Image
+                source={{
+                  uri: "https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg",
+                }}
+                alt="image"
+                resizeMode="cover"
+              />
+            </AspectRatio>
+          </Box>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        snapToInterval={width}
+        snapToAlignment="start"
+        scrollEventThrottle={16}
+        onMomentumScrollEnd={(event) => {
+          const index = Math.floor(
+            event.nativeEvent.contentOffset.x /
+              event.nativeEvent.layoutMeasurement.width
+          );
+          setPagination(index + 1);
+        }}
+      />
+      <Box position="absolute" bottom={2} bg="gray.800" px={2} rounded="full">
+        <HStack>
+          <Text style={{ color: "lightgray", fontSize: 14 }}>
+            {pagination}/{randomArray.length}
+          </Text>
+        </HStack>
+      </Box>
+    </Center>
   );
 }
